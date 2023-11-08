a1a225917c2365e2bc734420002fadf12e6b7c80
AnnRozhkina <annrozhkina@gmail.com>
2023-11-08 12:19:17 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './styles.scss';
+
+
+const onClickEvent = (e) => {
+    e.preventDefault();
+    alert('You Clicked Me!')
+}
+
+const App = () => {
+    return (
+        <div className={styles.content}>
+            <div className={styles.label}>
+                Create React App Without CRA😊
+            </div>
+            <button className={styles.btn} onClick={onClickEvent}>Click Me 😎</button>
+        </div>
+    )
+}
+
+export default App
